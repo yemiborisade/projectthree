5fdc8263bdc10bf4b98b6ea36944354a2c86bbe8
Akinyemi Borisade <yemiborisade@akinyemis-mbp.attlocal.net>
2020-03-19 22:00:20 -0400

    second page started

--- threeproject/client/src/App.js
+++ threeproject/client/src/App.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './home'
+import PageTwo from './pagetwo'
 
 const App = (props) => {
   return (
-    <div className="App">
-      <h1>Issa. Vibe.</h1>
-      <Container className="theForm">
-    <Form>
-      <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="Enter.Email.Here." />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="Enter.Password.Here." />
-      </FormGroup>
-      </Form>
-      </Container>
-      <Button> Submit.</Button>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Container className="issaWelcome">
-      <p>Welcome to Issa Vibe. Whether it's a Friday night out on the town with friends or Saturday morning filled with chores, Issa Vibe can provide you with the music to put you in the right mood. Join now to catch a vibe. </p>
-      </Container>    
-      </div>
+
+      <Router>
+      <Switch>
+      <Route exact path="/" component = {Home}/>
+      <Route exact path="/pagetwo" component = {PageTwo}/>
+      </Switch>
+      </Router>  
   );
 }
 
---
+++ threeproject/client/src/pagetwo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+
+function PageTwo() {
+    return (
+        <div className="App">
+        <h1>Issa. Vibe.</h1>
+        <Container className="theForm">
+
+
+        </Container>
+
+        </div>
+
+ 
+
+      
+    );
+  }
+  
+  export default PageTwo;
