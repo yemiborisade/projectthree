bdc958e257693106dafdf88ce91df72bfb515e2a
Akinyemi Borisade <yemiborisade@akinyemis-mbp.attlocal.net>
2020-03-19 20:28:29 -0400

    welcome page made

--- threeproject/client/src/App.js
+++ threeproject/client/src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Issa. Vibe.</h1>
+      <Container className="theForm">
+    <Form>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input type="email" name="email" id="exampleEmail" placeholder="Enter.Email.Here." />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input type="password" name="password" id="examplePassword" placeholder="Enter.Password.Here." />
+      </FormGroup>
+      </Form>
+      </Container>
+      <Button> Submit.</Button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Container className="issaWelcome">
+      <p>Welcome to Issa Vibe. Whether it's a Friday night out on the town with friends or Saturday morning filled with chores, Issa Vibe can provide you with the music to put you in the right mood. Join now to catch a vibe. </p>
+      </Container>    
+      </div>
   );
 }
 
